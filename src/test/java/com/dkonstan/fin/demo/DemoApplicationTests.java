af41fa9a8683d7d535f23342432cc15f5c5f5c45
dkonstan <dkkonstantopoulos@yahoo.gr>
2021-12-23 22:52:42 +0200

    init

---
+++ src/test/java/com/dkonstan/fin/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkonstan.fin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
